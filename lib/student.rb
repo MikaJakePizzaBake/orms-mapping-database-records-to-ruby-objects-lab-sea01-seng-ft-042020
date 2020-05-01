4d942bc6717370a2be067638ecc54fb18111f58a
Trevor Low <tlow94@uw.edu>
2020-05-01 01:46:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,19 +13,11 @@ end
 def self.drop_table
 DB[:conn].execute("DROP TABLE IF EXISTS students")
 end
-def self.find_by_name(name)
-  # find the student in the database given a name
-  # return a new instance of the Student class
+def self.find_by_name(row)
   sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT 1
+  SELECT*FROM students WHERE name = ? LIMIT 1
   SQL
-
-  DB[:conn].execute(sql,name).map do |row|
-    self.new_from_db(row)
-  end.first
+  DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
 end
 def self.all
   sql = <<-SQL
