bc7d4fd772e2762b3a27dcdfec4f746450774b15
Trevor Low <tlow94@uw.edu>
2020-05-01 02:04:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,4 +55,10 @@ def self.first_student_in_grade_10
   SQL
   DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
 end
+def self.all_students_in_grade_X
+  sql = <<-SQL
+  SELECT * FROM students WHERE grade == ?
+  SQL
+  DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+end
 end
