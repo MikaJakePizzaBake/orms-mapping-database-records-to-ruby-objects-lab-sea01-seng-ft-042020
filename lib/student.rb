d96b14cb8ddc1aaa00d54466c7ea75e39b22dd73
Trevor Low <tlow94@uw.edu>
2020-05-01 01:41:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,12 @@ def self.new_from_db(row)
   new_student.grade = row[2]
   new_student
 end
+def self.create_table
+  DB[:conn].execute("CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)")
+end
+def self.drop_table
+DB[:conn].execute("DROP TABLE students")
+end
 def self.find_by_name
   sql = <<-SQL
   SELECT*FROM students WHERE name = ? LIMIT 1
